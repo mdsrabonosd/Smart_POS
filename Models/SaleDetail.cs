ff9487bfdbd4428a7d569a4f52ed1fd1a68fe8ad
Md.Srabon <mdsrabon.cse@gmail.com>
2026-09-06 23:36:37 +0600

    Create SaleDetail.cs

---
+++ Models/SaleDetail.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartPOS.Models
+{
+    public class SaleDetail
+    {
+        public int Id { get; set; }
+
+        // Foreign Key to Sale
+        [Required]
+        public int SaleId { get; set; }
+        [ForeignKey("SaleId")]
+        public virtual Sale? Sale { get; set; }
+
+        // Foreign Key to Product
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product? Product { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal UnitPrice { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal TotalPrice { get; set; } // Quantity * UnitPrice
+    }
+}
